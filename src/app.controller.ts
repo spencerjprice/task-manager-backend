549d6adfbcb86212562149fcc48c3d4edd1784b3
Spencer Price <spencerprice@Spencers-Laptop.local>
2023-05-17 08:25:03 -0600

    Added basic logging

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,8 +1,10 @@
 import {
+    BadRequestException,
     Body,
     Controller,
     Delete,
     Get,
+    Logger,
     Param,
     Post,
     Put,
@@ -14,20 +16,34 @@ import { AppService } from './app.service';
 
 @Controller('/tasks')
 export class AppController {
+    private readonly logger = new Logger(AppController.name);
+
     constructor(private readonly appService: AppService) {}
 
     @Get()
     @ApiOperation({ summary: 'Get all tasks' })
     @ApiResponse({ status: 200, description: 'All the tasks' })
     getTasks(): Promise<Task[]> {
-        return this.appService.getTasks();
+        try {
+            this.logger.log('Getting all tasks');
+            return this.appService.getTasks();
+        } catch (error) {
+            this.logger.error('Error getting all tasks', { error });
+            throw new BadRequestException(error.message);
+        }
     }
 
     @Post()
     @ApiOperation({ summary: 'Create a task' })
     @ApiResponse({ status: 200, description: 'The created task' })
     createTask(@Body() body: TaskDto): Promise<Task> {
-        return this.appService.createTask(body);
+        try {
+            this.logger.log('Creating a task');
+            return this.appService.createTask(body);
+        } catch (error) {
+            this.logger.error('Error creating a task', { error });
+            throw new BadRequestException(error.message);
+        }
     }
 
     @Put(':id')
@@ -42,7 +58,13 @@ export class AppController {
         @Param('id') id: string,
         @Body() body: Partial<TaskDto>,
     ): Promise<Task> {
-        return this.appService.updateTask(+id, body);
+        try {
+            this.logger.log('Updating a task');
+            return this.appService.updateTask(+id, body);
+        } catch (error) {
+            this.logger.error('Error updating a task', { error });
+            throw new BadRequestException(error.message);
+        }
     }
 
     @Delete(':id')
@@ -54,7 +76,13 @@ export class AppController {
     })
     @ApiResponse({ status: 200, description: 'The deleted task' })
     deleteTask(@Param('id') id: string): Promise<Task> {
-        return this.appService.deleteTask(+id);
+        try {
+            this.logger.log('Deleting a task');
+            return this.appService.deleteTask(+id);
+        } catch (error) {
+            this.logger.error('Error deleting a task', { error });
+            throw new BadRequestException(error.message);
+        }
     }
 
     @Get(':id')
@@ -66,6 +94,12 @@ export class AppController {
     })
     @ApiResponse({ status: 200, description: 'The task' })
     getTask(@Param('id') id: string): Promise<Task> {
-        return this.appService.getTask(+id);
+        try {
+            this.logger.log('Getting a task');
+            return this.appService.getTask(+id);
+        } catch (error) {
+            this.logger.error('Error getting a task', { error });
+            throw new BadRequestException(error.message);
+        }
     }
 }
